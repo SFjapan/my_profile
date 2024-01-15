2dbf8d4831af0635e220e6d41dea02e178687392
SFjapan <fs12265669@gmail.com>
2024-01-15 16:26:44 +0900

    commit microcms get blogs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,28 @@
-export default function Index(){
-    return(
-        <div>test</div>
-    )
+import { client } from "../libs/client";
+
+export default function Home({ blogs}) {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <ul>
+        {blogs.map((blog) => (
+          <li className="mb-2" key={blog.id}>
+            <a>{blog.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blogs" });
+
+
+  return {
+    props: {
+      blogs: data.contents,
+    },
+  };
+};
