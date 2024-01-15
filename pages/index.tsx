da377761c4d7ed7d017f5b63035f055c4576078c
SFjapan <fs12265669@gmail.com>
2024-01-15 16:28:27 +0900

    test vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,5 @@
-import { client } from "../libs/client";
-
-export default function Home({ blogs}) {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <ul>
-        {blogs.map((blog) => (
-          <li className="mb-2" key={blog.id}>
-            <a>{blog.title}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+export default function Index(){
+    return(
+        <div>test vercel</div>
+    )
 }
-
-
-// データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blogs" });
-
-
-  return {
-    props: {
-      blogs: data.contents,
-    },
-  };
-};
